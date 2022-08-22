b7fc78237b5179920285d7e8216dc0cf4cbe6541
KagazezhevB <91414564+KagazezhevB@users.noreply.github.com>
2022-08-22 23:38:30 +0300

    delete draft file

--- auto_changelog/draft.py
+++
@@ -1,9 +0,0 @@
-def is_part_in_list(str_, words):
-    for word in words:
-        if word.lower() in str_.lower():
-            return True
-    return False
-
-ignore = ["merge", "attempt", "OMG"]
-s = "Merge d"
-print(is_part_in_list(s, ignore))
